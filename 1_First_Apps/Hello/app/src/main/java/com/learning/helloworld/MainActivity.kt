3393c1318c4c9c489222808fc90d0c72e3b6d3be
Martin Mallet <martinmallet32@gmail.com>
2020-01-09 18:43:51 -0400

    1.2 Coding Challenge

--- 1_First_Apps/Hello/app/src/main/java/com/learning/helloworld/MainActivity.kt
+++ 1_First_Apps/Hello/app/src/main/java/com/learning/helloworld/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.learning.helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener { countUp() }
+    }
+
+    private fun rollDice(){
+        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        val result: String = resultText.text.toString()
+
+        if(result.toIntOrNull() != null && result.toInt() < 6){
+            resultText.text = (result.toInt() + 1).toString()
+        }
     }
 }
