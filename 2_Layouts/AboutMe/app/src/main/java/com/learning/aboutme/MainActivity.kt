e938c8e137db3e010eb5d4696e99518ae6959a88
Martin Mallet <martinmallet32@gmail.com>
2020-01-12 18:26:51 -0400

    2.1 Done

--- 2_Layouts/AboutMe/app/src/main/java/com/learning/aboutme/MainActivity.kt
+++ 2_Layouts/AboutMe/app/src/main/java/com/learning/aboutme/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.learning.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
