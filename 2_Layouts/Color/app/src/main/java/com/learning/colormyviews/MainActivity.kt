778aefe0812a0c724cf14c2a1fd5d367a0413350
Martin Mallet <martinmallet32@gmail.com>
2020-01-20 19:12:06 -0400

    2.3 Color Done

--- 2_Layouts/Color/app/src/main/java/com/learning/colormyviews/MainActivity.kt
+++ 2_Layouts/Color/app/src/main/java/com/learning/colormyviews/MainActivity.kt
@@ -3,8 +3,10 @@ package com.learning.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +27,17 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
 
+            R.id.imageView2 -> view.setBackgroundResource(R.drawable.ic_launcher_foreground)
+            R.id.imageView3 -> view.setBackgroundResource(R.drawable.ic_launcher_foreground)
+            R.id.imageView4 -> view.setBackgroundResource(R.drawable.ic_launcher_foreground)
+            R.id.imageView5 -> view.setBackgroundResource(R.drawable.ic_launcher_foreground)
+
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
 
@@ -38,12 +51,23 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                imageView2, imageView3, imageView4,
+                imageView5, redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
